a11f0859a22d0f820b587517eb3edc9d962dca5b
Gustavo Theotonio <gustavotheotonio46@gmail.com>
2023-03-27 22:36:51 -0300

    finish aplication in html css js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const api = {
+    key: '5d9ff841d602718c14e5eb161f39e8c3',
+    url: 'https://api.openweathermap.org/data/2.5/',
+    lang: 'pt_br',
+    units: 'metric'
+}
+
 const button = document.querySelector('.buttonSearch');
 const inputCity = document.querySelector('.inputCity');
 const city = document.querySelector('.city');
@@ -7,48 +14,122 @@ const day = document.querySelector('.actualDay');
 const time = document.querySelector('.time');
 const drop = document.querySelector('.drop');
 const wind = document.querySelector('.wind');
+const description = document.querySelector('.description');
 const img = document.querySelector('#imgTemp');
+const body = document.querySelector('body')
 
-const apiKey = ''
 
-async function getWeather(){
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&units=metric&appid=${apiKey}&lang=pt_br`, {
+function veryTimeModifcTheme(){
+    const hours = new Date().getHours()
+
+    if(hours >= 18){
+        body.classList.toggle('onDarkMode')
+    }else{
+        body.classList.remove('onDarkMode')
+    }
+}
+
+veryTimeModifcTheme()
+window.addEventListener('load', geolocation)
+
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(successCallback, erroCallback) 
+    }else{
+        alert('Your navigator not supported geolocation')
+    }
+
+    function successCallback(position){
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        getWeatherLatAndLon(lat, long)
+    }
+
+    function erroCallback(error){
+        return error
+    }
+}
+
+async function getWeatherLatAndLon(lat, long){    
+    const res = await fetch(`${api.url}weather?lat=${lat}&lon=${long}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`, {
         method: 'GET',
         mode: "cors",
         cache: 'default'
     })
     const data = await res.json()
-    return data;
+    viewInDinsplay(data)
+    
 }
 
+button.addEventListener('click', handleClick);
 function handleClick(e){
     e.preventDefault()
 
-    viewInDinsplay()
-    clearValue();
+    getWeather(city.value)
+    clearValue()
+} 
+
+async function getWeather(){
+    const res = await fetch(`${api.url}weather?q=${inputCity.value}&lang=${api.lang}&units=${api.units}&appid=${api.key}`, {
+        method: 'GET',
+        mode: "cors",
+        cache: 'default'
+    })
+
+    const data = await res.json()
+    viewInDinsplay(data)
 }
 
-async function viewInDinsplay(){
-    const data = await getWeather()
+async function viewInDinsplay(data){
+    const semana = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date()
 
-    if(data.weather[0].main == 'Rain'){
-        img.setAttribute('src', './assets/rainyDay.svg')
+    if(body.classList.contains('onDarkMode')){
+        switch(data.weather[0].main){
+            case 'Rain':
+                img.setAttribute('src', './assets/rainyNight.svg')
+                break;
+            case 'Clouds': 
+                img.setAttribute('src', './assets/noite-nublada.svg')
+                break;
+            case 'Clear':
+                img.setAttribute('src', './assets/night.svg')
+                break;
+            default:
+                img.setAttribute('src', './assets/weather.svg')
+        }
+    }else{
+        switch(data.weather[0].main){
+            case 'Rain':
+                img.setAttribute('src', './assets/rainyDay.svg')
+                break;
+            case 'Clouds': 
+                img.setAttribute('src', './assets/nublado.svg')
+                break;
+            case 'Clear':
+                img.setAttribute('src', './assets/sun.svg')
+                break;
+            default:
+                img.setAttribute('src', './assets/weather.svg')
+        }
     }
 
-    console.log(data.weather[0].main)
-
+    description.innerHTML = data.weather[0].description
     city.innerText = data.name
+    day.innerHTML = `${semana[date.getDay()]},`
+    time.innerHTML = `${date.getHours()}:${date.getMinutes()}` 
     country.innerHTML = data.sys.country
-    temp.innerHTML = parseInt(data.main.temp) 
-    drop.innerHTML = data.main.humidity
+    temp.innerHTML = `${parseInt(data.main.temp)}°`
+    drop.innerHTML = `${data.main.humidity}%`
     wind.innerHTML = `${data.wind.speed}Km/h`
-    day.innerHTML = data.lastupdate.value
-
 }
 
 function clearValue(){
     document.querySelector('.inputCity').value = ''
 }
+let date = new Date()
+console.log(date.getHours(),':',date.getMinutes())
 
-console.log(getWeather())
-button.addEventListener('click', handleClick);
+
+
+
